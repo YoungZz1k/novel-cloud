5f2db253d944b5773578ff8296f5007476572d82
YoungZz1k <907175262@qq.com>
2025-04-19 21:24:14 +0800

    feat(core): 添加流量限制和防刷功能
    - 新增 FlowLimitInterceptor 拦截器实现接口防刷和限流
    - 添加 IpUtils 工具类用于获取 IP 地址
    - 在 WebConfig 中注册流量限制拦截器- 更新 pom.xml 添加 Sentinel 相关依赖

---
+++ novel-core/novel-config/src/main/java/com/youngzz1k/novel/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.youngzz1k.novel.config;
+
+import com.youngzz1k.novel.common.constant.ApiRouterConsts;
+import com.youngzz1k.novel.config.interceptor.FlowLimitInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private final FlowLimitInterceptor flowLimitInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 流量限制拦截器
+        registry.addInterceptor(flowLimitInterceptor)
+                .addPathPatterns("/**")
+                .order(0);
+    }
+}
